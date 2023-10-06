d25bbdb41a02443d33fb44464a66bc48250c03c8
Karansing <rkaransing13@gmail.com>
2023-10-06 10:59:51 +0530

    Sorting using functional interface and lambda functions.

---
+++ java8/src/main/java/lambda_functions/SortingUsingFunctionalInterfaceAndLambdaFunction.java
@@ -0,0 +1,17 @@
+package lambda_functions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SortingUsingFunctionalInterfaceAndLambdaFunction {
+    public static void main(String[] args) {
+        ArrayList<Integer> integerList = new ArrayList<>(Arrays.asList(66, 77, 11, 0, 22));
+        System.out.println("Before sorting : " + integerList);
+
+        Comparator<Integer> comparator = (firstInteger, secondInteger) -> (firstInteger < secondInteger) ? -1 : (firstInteger > secondInteger) ? 1 : 0;
+        integerList.sort(comparator);
+
+        System.out.println("After sorting : " + integerList);
+    }
+}
